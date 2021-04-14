754bfe58bd21b9e1b4d5a4970806e9b852922bb9
Waheed Khan <wahid.khan74@gmail.com>
2021-04-14 06:25:38 +0000

    add ssl to spring boot app

---
+++ src/main/java/com/dell/webservice/Phase3SslHandlingWebservice04142022Application.java
@@ -0,0 +1,13 @@
+package com.dell.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3SslHandlingWebservice04142022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3SslHandlingWebservice04142022Application.class, args);
+	}
+
+}
